c96ae1d925c722535628ad9d6b2b47d98c77d12f
bnjddi <si1981mo@gmail.com>
2018-12-19 22:36:12 +0100

    ok

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function onSignIn(googleUser)
 {
 	var profile=googleUser.getBasicProfile();
 	$(".g-signin2").css("display","none");
-	$(".g-signin2").css("display","block");
+	$(".signin").css("display","block");
 	$("#pic").attr('src',profile.getImageUrl());
 	$("#email").text(profile.getEmail());
 }
