4bee43b027529bbc72b6d9888e21868792b100a4
bnjddi <si1981mo@gmail.com>
2018-12-19 22:34:17 +0100

    ok

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 function onSignIn(googleUser)
 {
 	var profile=googleUser.getBasicProfile();
+	$(".g-signin2").css("display","none");
 	$(".g-signin2").css("display","block");
 	$("#pic").attr('src',profile.getImageUrl());
 	$("#email").text(profile.getEmail());
